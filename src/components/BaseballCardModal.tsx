acd95da01fd643516fa13bb91fbd7727309ed53c
trekker10 <trekr3000@gmail.com>
2026-06-19 21:36:39 -0600

    Rename card button to 'See [Name]'s Baseball Card'

--- src/components/BaseballCardModal.tsx
+++ src/components/BaseballCardModal.tsx
@@ -201,7 +201,7 @@ export default function BaseballCardModal(props: BaseballCardProps) {
           <circle cx="18" cy="5" r="3"/><circle cx="6" cy="12" r="3"/><circle cx="18" cy="19" r="3"/>
           <line x1="8.6" y1="10.5" x2="15.4" y2="6.5"/><line x1="8.6" y1="13.5" x2="15.4" y2="17.5"/>
         </svg>
-        Share {props.first}&apos;s Card
+        See {props.first}&apos;s Baseball Card
       </button>
 
       {/* Modal backdrop */}
